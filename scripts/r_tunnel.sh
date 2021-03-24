6090a83602d4dc7d93361af5663122eb28f7743d
CzD <czurko.dani@edu.bme.hu>
2021-03-24 18:36:26 +0100

    Creating tmux session in the r_tunnel.sh bash script

--- scripts/r_tunnel.sh
+++ scripts/r_tunnel.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
 ip=10.0.0.3
+echo "Creating tmux session..."
+echo "Attache to the session with below command:"
+echo "tmux a -t home_session"
+tmux new -d -s "home_session"
 echo "Building tunnel to: $ip"
 ssh -R 10022:127.0.0.1:22 agile@$ip
