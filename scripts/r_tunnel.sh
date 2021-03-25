4a9c32092566bed2eca8fb84927a21705c1cdc04
CzD <czurko.dani@edu.bme.hu>
2021-03-25 16:22:16 +0100

    r_tunnel.sh update, detaching from session

--- scripts/r_tunnel.sh
+++ scripts/r_tunnel.sh
@@ -4,6 +4,8 @@ ip=10.0.0.3
 echo "Creating tmux session..."
 echo "Attach to the session with the below command:"
 echo "tmux a -t home_session"
+echo "Detaching from tmux session:"
+echo "Type: Ctrl+b d"
 tmux new -d -s "home_session"
 echo "Building tunnel to: $ip"
 ssh -R 10022:127.0.0.1:22 agile@$ip
