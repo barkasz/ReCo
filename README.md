e5f1e848454104f21d008722800b17705f45a56b
Kovács Levente <kovacslevi0@gmail.com>
2021-05-08 22:27:56 +0200

    barrier + dual tunnel

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ README.md szerkesztéséhez segítségek:
 https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
 https://guides.github.com/features/mastering-markdown/
 
-# Tunnel building
+# Remote control
 
 From *HOME_PC* (which we want to access remotely) run the following command to build the remote port forwarding tunnel to *CLOUD*. The command forwards a port to the ssh server of the home PC.
 ```
--- scripts/remotepc.sh
+++ scripts/remotepc.sh
@@ -5,6 +5,8 @@ tmux new -d -s stream "nc -l 19000 | ffplay -fflags nobuffer -"
 ip=152.66.246.223
 echo "Connecting...."
 echo "Building tunnel to: $ip"
-ssh -f -R 19000:127.0.0.1:19000 -p42222 tunneler@$ip "sleep 10"
+ssh -f -R 19000:127.0.0.1:19000 -R 24800:127.0.0.1:24800 -p42222 tunneler@$ip "sleep 10"
+
+barriers --daemon
 
 ssh -J tunneler@152.66.246.223:42222 agile@127.0.0.1 -p 40000 "/bin/bash /home/agile/ReCo/scripts/homepc.sh"
